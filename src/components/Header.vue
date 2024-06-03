2f115ab47ba5aee8f653ec4879299e82bd76c485
natdm02 <natalidimarino2002@gmail.com>
2024-06-03 23:39:41 +0200

    [add] install fontawesome + route components

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,13 +12,18 @@ export default {
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="collapse navbar-collapse ms-4" id="navbarNav">
                 <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#">Home</a>
+                    <li class="nav-item mx-2">
+                        <router-link :to="{name: 'home'}">
+                            <i class="fa-solid fa-house-chimney fa-lg">
+                                <span class="page_name text-dark">Home</span>
+                            </i>
+                        </router-link>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#">Admin</a>
+                    <li class="nav-item mx-2">
+                        <router-link :to="{name: 'projects'}"><i class="fa-solid fa-list-check fa-lg"><span class="page_name text-dark">Projects</span></i></router-link>
+                        
                     </li>
                 </ul>
             </div>
@@ -28,4 +33,27 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+header {
+        .navbar {
+            .container-fluid {
+                .navbar-collapse {
+                    .nav-item {
+                        &:hover {
+                            transform: scale(1.2);
+                            transition: all 0.3s;
+                        }
+                        i {
+                            .page_name {
+                                font-size: .7rem;
+                                margin-left: 5px;
+                            }
+                            &.fa-list-check {
+                                margin-top: 13px;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 </style>
