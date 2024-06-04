888a20ecd72b18f3e574b892684586153ddbe8d3
natdm02 <natalidimarino2002@gmail.com>
2024-06-04 22:52:38 +0200

    [add] sendform

--- src/components/partials/ContactForm.vue
+++ src/components/partials/ContactForm.vue
@@ -1,9 +1,59 @@
 <script>
+import axios from 'axios';
+import { store } from '../../data/store';
+
 export default {
-    name: 'ContactForm'
+    name: 'ContactForm',
+    data(){
+        return{
+            name: '',
+            email: '',
+            message: '',
+        }
+    },
+    methods: {
+        sendForm(){
+            const data = {
+                name: this.name,
+                email: this.email,
+                message: this.message
+            }
+            console.log(data);
+        }
+    }
 }
 </script>
 
 <template>
+<div class="container rc-form rounded-2">
+    <form @submit.prevent="sendForm()" class="p-3">
 
+            <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input v-model.trim="name" type="text" name="name" id="" class="form-control" placeholder="Type your Fullname">
+            </div>
+
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input v-model.trim="email" type="email" name="email" id="" class="form-control" placeholder="Type your Email">
+            </div>
+
+            <div class="form-floating mb-4">
+                <p>Message</p>
+                <textarea v-model.trim="message" name="message" id="" class="form-control" style="height: 150px; width: 100%;"></textarea>
+            </div>
+
+            <div class="d-flex justify-content-center">
+                <button type="submit" class="btn btn-primary">Send <i class="fa-solid fa-paper-plane fa-lg"></i></button>
+            </div>
+        </form>
+    </div>
 </template>
+
+
+
+<style lang="scss" scoped>
+    .rc-form {
+        box-shadow: 0 0 10px rgba($color: #000000, $alpha: 0.1);
+    }
+</style>
--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -16,6 +16,7 @@ export default {
 
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus provident sequi tenetur modi iste! Tenetur veniam quaerat consectetur culpa laborum, nemo cum officiis aut a labore veritatis eum voluptas, odio explicabo accusamus doloribus quo aspernatur asperiores deleniti optio error nostrum. Id suscipit necessitatibus quia, sint nesciunt earum ex nam aliquam. Totam, corporis amet consequuntur rem neque commodi reiciendis est aperiam exercitationem similique.</p>
         </div>
+        <ContactForm/>
 </template>
 
 
