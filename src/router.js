a2c0c01932ba2dcba623b51597e4b2d0f3b17aa0
natdm02 <natalidimarino2002@gmail.com>
2024-06-04 15:47:18 +0200

    [add] projectdetail in router

--- src/router.js
+++ src/router.js
@@ -2,21 +2,31 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import Home     from './pages/Home.vue';
 import Projects from './pages/Projects.vue';
+import ProjectDetails from './pages/ProjectDetails.vue';
 
 const router = createRouter ({
     history: createWebHistory(),
 
     routes: [
+
         {
             path: '/',
             name: 'home',
             component: Home
         },
+
         {
             path: '/projects',
             name: 'projects',
             component: Projects
-        }
+        },
+
+        {
+            path :'/details/:slug',
+            name: 'projectdetails',
+            component : ProjectDetails
+        },
+        
     ]
 })
 
