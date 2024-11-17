8d43356366f92706831ec8e6d78971de87b9a410
natdm02 <natalidimarino2002@gmail.com>
2024-11-17 22:54:45 +0100

    [add] basic scaffolding

---
+++ src/components/ProjectList.vue
@@ -0,0 +1,30 @@
+<script>
+
+export default {
+  data() {
+    return {
+    };
+  },
+};
+
+    
+
+
+
+</script>
+
+<template>
+
+
+
+  <div>
+    <h1>projects</h1>
+  </div>
+
+
+
+</template>
+
+<style>
+
+</style>
