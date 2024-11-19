fbaa770daaf4d133059f9c8744d380f454fa47bf
natdm02 <natalidimarino2002@gmail.com>
2024-11-19 21:31:02 +0100

    [add] update router-link

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,13 @@ export default {
 <template>
 
   <div id="app">
-    <ProjectList />
+    
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/projects">Progetti</router-link>
+    </nav>
+
+    <router-view></router-view>
   </div> 
 
 </template>
