62c95964c6e2dbc832bf0a643767a25afdad3499
natdm02 <natalidimarino2002@gmail.com>
2024-12-23 16:29:37 +0100

    [add] link registration

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,12 @@ export default {
     
     <nav>
       <router-link to="/">Home</router-link>
+      <br>
       <router-link to="/projects">Progetti</router-link>
+      <br>
       <router-link to="/github-projects">GitHub Projects</router-link>
+      <br>
+      <a href="http://127.0.0.1:8000/" target="_blank">Register</a>
     </nav>
 
     <router-view></router-view>
